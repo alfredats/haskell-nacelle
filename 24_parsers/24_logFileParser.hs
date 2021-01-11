fb7ab53d3e80c8a6029002752d27dd8fe5849175
Alfred A <20140832+carrotsnotfound@users.noreply.github.com>
2021-01-11 15:47:47 +0800

    WIP: Ch24 exercise 5.
    - Complete Date Header parser
    - Complete Record-time parser
    - Complete Record parser
    
    At this point, the parser for an entire day (date header + records)
    is still not working. Will look to fix soon.

--- 24_parsers/24_logFileParser.hs
+++ 24_parsers/24_logFileParser.hs
@@ -5,6 +5,7 @@ module LogFileParser where
 
 import Control.Applicative
 
+import Data.Map as M
 import Text.Trifecta
 import Text.RawString.QQ
 
@@ -20,15 +21,19 @@ skipWhitespace = skipMany (oneOf "\t ")
 commentEx :: String 
 commentEx = "-- wheee a comment"
 
-testComment = [r|
+commentEx' :: String
+commentEx' = [r|
 -- whee a comment ;
 
-# header
+# 2020-01-01 
 |]
 
 commentParse :: Parser () 
 commentParse = 
-  skipEOL >> char '-' >> char '-' >> skipMany alphaNum >> skipEOL
+    skipEOL >> 
+      string "--" >> 
+        skipMany (noneOf "\n") >> 
+          skipEOL
 
 gotoEOL :: Parser ()
 gotoEOL = try commentParse <|> skipEOL
@@ -46,8 +51,14 @@ headerEx = "# 2025-02-05"
 headerEx' :: String 
 headerEx' = "# 2025-02-07 -- dates not necessary"
 
+headerEx'' :: String
+headerEx'' = [r|
+# 2020-02-08 -- dates not necessarily sequential
+|]
+
 headerParse :: Parser Header
 headerParse = do
+  skipEOL
   _ <- char '#'
   skipWhitespace
   y <- some digit 
@@ -55,9 +66,9 @@ headerParse = do
   m <- some digit
   _ <- char '-'
   d <- some digit
-  skipOptional commentParse
-  skipEOL
+  gotoEOL
   return $ Header (read y) (read m) (read d)
+
 
 
 
@@ -66,8 +77,7 @@ type Minute = Integer
 data Time = Time Hour Minute deriving (Eq, Ord, Show)
 
 type Activity = String
-
-data Record = Record Time Activity deriving Show
+type Records = Map Time Activity 
 
 recordEx :: String
 recordEx = "09:00 Sanitizing moisture collector"
@@ -75,31 +85,122 @@ recordEx = "09:00 Sanitizing moisture collector"
 recordEx' :: String
 recordEx' = "08:00 Breakfast -- should I try skippin bfast?"
 
+recordEx'' :: String
+recordEx'' =
+  "11:00 Exercising in high-grav gym -- note the '-' in high-grav"
 timeParse :: Parser Time
 timeParse = do
   h <- some digit
   _ <- char ':'
   m <- some digit
+  skipWhitespace
   return $ Time (read h) (read m)
 
 activityParse :: Parser Activity
 activityParse = do
   skipWhitespace
-  token (some (noneOf "-"))
+  sepEndBy anyChar (commentParse <|> skipEOL)
+-- I'm not quite satisfied with the parsing output of 
+-- activityParse with recordEx'. There is a trailing whitespace
+-- character contained in the output. Perhaps this should be
+-- accounted for within the comment parser?
 
-recordParse :: Parser Record
+recordParse :: Parser (Time, Activity) 
 recordParse = do
   t <- timeParse
   a <- activityParse
+  return $ (,) t a
+
+
+
+dayEx :: String
+dayEx = [r|
+# 2025-02-05
+08:00 Breakfast
+09:00 Sanitizing moisture collector
+|]
+
+dayEx' :: String
+dayEx' = [r|
+# 2025-02-07 -- dates not nececessarily sequential
+08:00 Breakfast -- should I try skippin bfast?
+09:00 Bumped head, passed out
+13:36 Wake up, headache
+|]
+
+dayEx'' :: String
+dayEx'' = [r|
+# 2025-02-05
+08:00 Breakfast
+09:00 Sanitizing moisture collector
+11:00 Exercising in high-grav gym -- note the '-' in high-grav
+12:00 Lunch
+13:00 Programming
+17:00 Commuting home in rover
+17:30 R&R
+19:00 Dinner
+21:00 Shower
+21:15 Read
+22:00 Sleep
+|]
+
+data DayLog = DayLog Header Records deriving (Eq, Show)
+
+getRecords :: DayLog -> Records
+getRecords (DayLog _ recs) = recs
+
+--instance Show DayLog where
+  --show (DayLog (Header y m d) workdone) = do
+    --show $ "# " <> show y <> "-" <> show m <> "-" <> show d
+
+dayLogParse :: Parser DayLog
+dayLogParse = do
+  skipEOL
+  h <- token headerParse <?> "parse date"
+  rs <- many recordParse <?> "parse records"
+  return $ DayLog h (M.fromList rs)
+
+
+
+logFileEx :: String 
+logFileEx = [r|
+-- wheee a comment
+# 2025-02-05
+08:00 Breakfast
+09:00 Sanitizing moisture collector
+11:00 Exercising in high-grav gym
+12:00 Lunch
+13:00 Programming
+17:00 Commuting home in rover
+17:30 R&R
+19:00 Dinner
+21:00 Shower
+21:15 Read
+22:00 Sleep
+
+# 2025-02-07 -- dates not nececessarily sequential
+08:00 Breakfast -- should I try skippin bfast?
+09:00 Bumped head, passed out
+13:36 Wake up, headache
+13:37 Go to medbay
+13:40 Patch self up
+13:45 Commute home for rest
+14:15 Read
+21:00 Dinner
+21:15 Read
+22:00 Sleep
+|]
+
+
+newtype LogFile = LogFile [DayLog] deriving Show
+
+logFileParse :: Parser LogFile
+logFileParse = do
+  skipEOL
+  commentParse
+  ds <- some dayLogParse
   gotoEOL
-  return $ Record t a
-
-
-
-
-
-
-
+  return $ LogFile ds
 
 maybeSuccess :: Result a -> Maybe a
 maybeSuccess (Success a) = Just a
@@ -108,10 +209,65 @@ maybeSuccess _ = Nothing
 test :: IO ()
 test = hspec $ do
   let ps p i = parseString p mempty i
-  describe "Log File parser" $ 
+  describe "comment parsing" $ do
     it "parses a single comment, and skips to first day" $ do
-      let m = ps (commentParse >> (char '#' <?> "parseHead") >> some letter) testComment
+      let m = ps (commentParse >> headerParse) commentEx'
           r = maybeSuccess m
-      r `shouldBe` Just "# header"
-          
-      
+      r `shouldBe` Just (Header 2020 01 01)
+  
+
+  describe "date header parsing" $ do
+    it "parses a single date" $ do
+      let m = ps headerParse headerEx
+          r = maybeSuccess m
+      r `shouldBe` Just (Header 2025 02 05)
+
+    it "parses a single date (with trailing comment)" $ do
+      let m = ps headerParse headerEx'
+          r = maybeSuccess m
+      r `shouldBe` Just (Header 2025 02 07)
+  
+
+  describe "record parser" $ do  
+    it "time parser returns time for single record" $ do
+      let m = parseString timeParse mempty recordEx
+          r = maybeSuccess m
+      r `shouldBe` Just (Time 9 0)
+    
+    it "record parser returns time & activity for single record" $ do
+      let m = parseString recordParse mempty recordEx
+          r = maybeSuccess m
+          expOut = 
+            Just ( Time 9 0
+                 , "Sanitizing moisture collector" :: Activity
+                 ) 
+      r `shouldBe` expOut
+    
+    it "record parser returns time & activity for single record with trailing comment" $ do
+      let m = parseString recordParse mempty recordEx'
+          r = maybeSuccess m
+          expOut = 
+            Just ( Time 8 0
+                 , "Breakfast " -- note the trailing whitespace
+                 )
+      r `shouldBe` expOut
+    it "record parser returns time & activity for single record with trailing comment, and containing single '-' in activity" $ do
+      let m = parseString recordParse mempty recordEx''
+          r = maybeSuccess m
+          expOut = 
+            Just ( Time 11 0
+                 , "Exercising in high-grav gym " -- once again, the trailing whitespace
+                 )
+      r `shouldBe` expOut
+  describe "dayLog parser" $ do
+    it "parses a single day, and returns a daylog" $ do
+      let m = parseString dayLogParse mempty dayEx'
+          r = maybeSuccess m
+          expOut = 
+            Just ( DayLog 
+                  (Header 2025 02 05)
+                  (fromList [ (Time 8 0, "Breakfast")
+                            , (Time 9 0, "Sanitizing moisture collector")
+                            ] )
+                )
+      r `shouldBe` expOut
