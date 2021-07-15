26e79d018f622feda0cea2b7434674128a43d842
Alfred A <20140832+alfredats@users.noreply.github.com>
2021-07-15 21:55:59 +0800

    Add logic + tests  handling player IO

--- 26_monadTransformers/Morra/src/Morra.hs
+++ 26_monadTransformers/Morra/src/Morra.hs
@@ -1,67 +1,95 @@
 module Morra where
 
+import System.IO
 import Control.Monad (forever)
 import System.Random
+import Control.Applicative
 import Control.Monad.Trans.State.Strict
+import Control.Monad.Trans.Reader
+import Text.Trifecta
 
 ---------------------------------------------------
 --- Types
 ---------------------------------------------------
-type MorraInt = Int 
-type MorraSum = Int 
+type MorraThrow = Integer
+type MorraSum = Integer
 
-type MorraScores = (Int, Int)
-type MorraGame = StateT MorraScores IO MorraInt
+data MorraIO = MorraInt Integer | MorraBound | MorraQuit deriving (Eq, Show)
 
+type MorraScores = (Integer, Integer)
+type Morra = StateT Game (ReaderT Config IO)
 
+data Gametype = PvC 
+newtype Config = Config { gametype :: Gametype }
 
+data Game = Game { gameScore :: MorraScores
+                 , pastThrows :: [MorraThrow]
+                 }
 ---------------------------------------------------
 --- Helpers 
 ---------------------------------------------------
-morraIntBounds :: (MorraInt,MorraInt)
-morraIntBounds = (0, 5)
+morraThrowBounds :: (MorraThrow, MorraThrow)
+morraThrowBounds = (0, 5)
 
 morraSumBounds :: (MorraSum, MorraSum)
 morraSumBounds = (0, 10)
 
-randomInt :: Int -> Int -> IO Int
-randomInt x y = do 
+randomInteger :: Integer -> Integer -> IO Integer
+randomInteger x y = do 
     g <- getStdGen
     return . fst $ randomR (x, y) g
 
-data Morra = Morra MorraInt [MorraInt] MorraInt
+checkBounds :: (Integer, Integer) -> MorraIO -> Maybe Integer
+checkBounds (f,l) (MorraInt x) 
+  | (x >= f) && (x <= l) = Just x
+  | otherwise = Nothing
+checkBounds _ _ = Nothing
 
-instance Eq Morra where
-    (==) (Morra s1 gs1 g1) (Morra s2 gs2 g2) =
-        (s1 == s2) && (gs1 == gs2) && (g1 == g2)
-
-
-computerGuesses :: IO (MorraInt, MorraSum)
-computerGuesses = do
-  rInt <- uncurry randomInt morraIntBounds
-  rSum <- randomInt rInt $ snd morraSumBounds
-  return (rInt, rSum)
-
-
-sumIsCorrect :: MorraSum -> MorraInt -> MorraInt -> Bool
+sumIsCorrect :: MorraSum -> MorraThrow -> MorraThrow -> Bool
 sumIsCorrect sum x y = (x+y) == sum
 
 
-handleGuess :: MorraGame -> MorraSum -> MorraInt -> IO MorraGame
-handleGuess morra pSum pInt = do
-    (cInt, cSum) <- computerGuesses 
-    putStrLn $ "The computer decided to throw" ++ show cInt ++ ", and guessed the sum to be " ++ show cSum
-    putStrLn $ "You decided to throw " ++ show pInt ++ ", and guessed the sum to be " ++ show pSum
+---------------------------------------------------
+--- IO & CPU 
+---------------------------------------------------
+cpuGuess :: IO (MorraThrow, MorraSum)
+cpuGuess = do
+  rInteger <- uncurry randomInteger morraThrowBounds
+  rSum <- randomInteger rInteger $ snd morraSumBounds
+  return (rInteger, rSum)
 
-    case (sumIsCorrect cSum cInt pInt, sumIsCorrect pSum cInt pInt) of 
-        (True, True) -> do
-            putStrLn "A draw!"
-            putStrLn $ "Current Score is " ++ show cScore ++ ", " ++ show pScore
-        (True, False) -> do
-          putStrLn "Computer wins!"
-        (False, True) -> do
-          putStrLn "You win!"
-        (_, _) -> do
-          putStrLn "Nobody guessed correctly, moving on..."
+parseMorra :: Parser MorraIO
+parseMorra = (MorraInt <$> integer) <|> (string "quit" >> return MorraQuit)
+
+askPlayerGetReply :: String -> IO String
+askPlayerGetReply i = putStr i >> getLine
+
+handlePlayerInput :: String -> (Integer, Integer) -> String -> IO MorraIO
+handlePlayerInput str bounds@(f,l) unparsed = do
+  case parseString parseMorra mempty unparsed of
+    Failure err -> putStrLn "Parse Error: Only integer characters or \"quit\" allowed" >> return MorraBound
+    Success MorraQuit -> return MorraQuit
+    Success parsed -> case checkBounds bounds parsed of
+      Nothing -> putStrLn ("Bounding error: Input must be between " ++ show f ++ " & " ++ show l) >> return MorraBound
+      Just x -> return $ MorraInt x
+
+
+-- handleGuess :: Game -> MorraSum -> MorraThrow -> IO Game
+-- handleGuess morra pSum pInteger = do
+--     (cInteger, cSum) <- cpuGuess
+--     putStrLn $ "The computer decided to throw" ++ show cInteger ++ ", and guessed the sum to be " ++ show cSum
+--     putStrLn $ "You decided to throw " ++ show pInteger ++ ", and guessed the sum to be " ++ show pSum
+--     putStrLn "What sum from 0-10 would you like to guess? \n"
+
+--     case (sumIsCorrect cSum cInteger pInteger, sumIsCorrect pSum cInteger pInteger) of 
+--         (True, True) -> do
+--             putStrLn "A draw!"
+--             putStrLn $ "Current Score is " ++ show cScore ++ ", " ++ show pScore
+--         (True, False) -> do
+--           putStrLn "Computer wins!"
+--         (False, True) -> do
+--           putStrLn "You win!"
+--         (_, _) -> do
+--           putStrLn "Nobody guessed correctly, moving on..."
 
         
--- 26_monadTransformers/Morra/test/Spec.hs
+++ 26_monadTransformers/Morra/test/Spec.hs
@@ -2,8 +2,18 @@ import Test.Hspec
 import Morra
 
 
-
-
 main :: IO ()
 main = hspec $ do
-    it ""
+    describe "handlePlayerInput" $ do
+        it "Returns MorraQuit on \"quit\"" $ do
+            m <- handlePlayerInput "" (0,0) "quit" 
+            m `shouldBe` MorraQuit
+        it "Returns MorraBound on erroneous string input" $ do
+            m <- handlePlayerInput "" (0,0) "123abc"
+            m `shouldBe` MorraBound
+        it "Returns MorraBound when input is out of bounds" $ do
+            m <- handlePlayerInput "" (0,10) "-10"
+            m `shouldBe` MorraBound
+        it "Returns MorraInt on proper input" $ do 
+            m <- handlePlayerInput "" (0,5) "04"
+            m `shouldBe` MorraInt 4
